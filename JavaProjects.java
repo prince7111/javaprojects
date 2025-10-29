d0e78e9a0177f85237d599c330c9bba46a27e493
prince7111 <princemeena7111@gmail.com>
2025-10-29 23:12:43 +0530

    java projects file (pnding)..

---
+++ JavaProjects.java
@@ -0,0 +1,9 @@
+package javaprojects;
+
+public class JavaProjects {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        //[project comming soon]
+    }
+    
+}
