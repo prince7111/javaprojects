9b4240bea1f449e13c2e151ad3d761abfb37702f
prince7111 <princemeena7111@gmail.com>
2025-10-31 21:19:56 +0530

    ||java projects || 2.pattern print||

--- JavaProjects.java
+++ JavaProjects.java
@@ -23,7 +23,21 @@ public class JavaProjects {
             break;
             default:System.out.print("not valid opertion (simle calC)");
         }
+    }   
+    public static void patterP(int n){
+        //print letter P
+         for(int i =0;i<n;i++){
+            for(int j =0;j<n;j++){
+                if(j==0 || (i<=(n/2)&& j==n/2)){
+                    System.out.print(" * ");
+                }else{
+                    System.err.print(" ");
+                }
+            }
+            System.err.println();
+        }
     }
+   
   
     public static void main(String args[]){
         System.err.println();   
