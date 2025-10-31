6702bb606a155b164cb7e282b3572b972024ae15
prince7111 <princemeena7111@gmail.com>
2025-10-31 21:18:14 +0530

    ||Java Projects || 1.Calulater console oriented||

--- JavaProjects.java
+++ JavaProjects.java
@@ -1,9 +1,34 @@
 package javaprojects;
 
+import com.sun.source.doctree.SystemPropertyTree;
+import java.util.Scanner;
+
 public class JavaProjects {
+    public static void Calc(int n){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Number here:");
+        int a =sc.nextInt();
+        System.out.println("Enter your number again:");
+        int b =sc.nextInt();
+        System.out.println("Enter:\n'+' for addi.\n'-'for subs."/*\n'*'for mul.\n'/'for div.\n"*/);
+        char operation = sc.next().charAt(0);
+        switch(operation){
+            case '+' :System.out.println(a+"+"+b+"="+(a+b));
+            break;
+            case '-' :System.out.print(a+"-"+b+"="+(a-b));
+            break;
+            case '*' :System.out.println("a*b="+(a*b));
+            break;
+            case '/' :System.out.print("a/b="+(a/b));
+            break;
+            default:System.out.print("not valid opertion (simle calC)");
+        }
+    }
+  
     public static void main(String args[]){
-        System.out.println("Hello World");
-        //[project comming soon]
+        System.err.println();   
     }
     
 }
+
+
